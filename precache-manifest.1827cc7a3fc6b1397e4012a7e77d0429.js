2897cba70dfadea43dc843f392c96ab0ae5cc87c
RLC <robz.l.c.77@gmail.com>
2019-01-26 02:27:32 -0600

    Updates

---
+++ precache-manifest.1827cc7a3fc6b1397e4012a7e77d0429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "8b233a0b39cae72aa0e1",
+    "url": "/clicky-game/static/js/main.8b233a0b.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "8b233a0b39cae72aa0e1",
+    "url": "/clicky-game/static/css/main.9451cd4a.chunk.css"
+  },
+  {
+    "revision": "3f9b28c44da0fe69468ea54d295fe25f",
+    "url": "/clicky-game/index.html"
+  }
+];
